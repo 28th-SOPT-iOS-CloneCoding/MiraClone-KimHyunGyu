a287f229915993b6d66a8cec50a409517c33b84c
hyungyu Kim <ypsophila97@icloud.com>
2021-09-04 22:34:58 +0900

    [#14] 카카오톡 Widget 구현
    ### Descriptions
    - 특정 위젯 클릭 시 특정 앱 내 페이지로 이동
    - widgetURL(_:) 메서드로 구현
    - AppDelegate 에서 URL 을 통해 앱 내 페이지로 이동
    
    ### Related Issue
    #14

--- KakaoQRcode-iOS-CloneCoding/KakaoQRcode-iOS-CloneCoding/Source/AppDelegate.swift
+++ KakaoQRcode-iOS-CloneCoding/KakaoQRcode-iOS-CloneCoding/Source/AppDelegate.swift
@@ -21,6 +21,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        let qrcodeLinkPath = "qrcode"
+        
+        // 정의된 URL 요청 외의 시도는 false.
+        guard let components = NSURLComponents(url: url, resolvingAgainstBaseURL: true), let path = components.path else {
+            return false
+        }
+        if path == qrcodeLinkPath {
+            let nextVC = QRCodeViewController()
+            nextVC.modalPresentationStyle = .overFullScreen
+            
+//            let window = UIWindow(frame: UIScreen.main.bounds)
+//            window.rootViewController = MainViewController()
+//            window.makeKeyAndVisible()
+//            self.window = window
+            
+            window?.rootViewController?.present(nextVC, animated: true, completion: nil)
+            return true
+        } else {
+            return false
+        }
+    }
 
 //    // MARK: UISceneSession Lifecycle
 //
